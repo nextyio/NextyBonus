b3dd471cd44c97fad4e3ab58027d85c271a53603
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-08-08 11:53:47 +0700

    remove setFixedPercent in func createLockedAmount, clean code

--- contracts/NextyBonus.sol
+++ contracts/NextyBonus.sol
@@ -5,10 +5,8 @@ import './SafeMath.sol';
 contract NextyBonus {
     using SafeMath for uint256;
     
-    //uint256 public constant BONUS_REMOVEALBE_DURATION= 180*24*60*60; // 180 days in second
-    //uint256 public constant LOCK_DURATION= 365*24*60*60; // 365 days in second
-    uint256 public constant BONUS_REMOVEALBE_DURATION= 5*60; // 180 days in second
-    uint256 public constant LOCK_DURATION= 10*60; // 365 days in second
+    uint256 public constant BONUS_REMOVEALBE_DURATION= 180*24*60*60; // 180 days in second
+    uint256 public constant LOCK_DURATION= 365*24*60*60; // 365 days in second
     
     uint256 public FIXED_PERCENT; //not constant
     uint256 public totalAmount= 0;
@@ -23,7 +21,7 @@ contract NextyBonus {
     }
 
     struct LockAtribute {
-        uint256 time;
+        uint256 startTime;
         uint256 endTime;
         uint256 value;
         StatusType lockStatus;
@@ -69,145 +67,146 @@ contract NextyBonus {
     //Owner's Functions
 
     constructor (uint256 _percent) public{ 
-        owner= msg.sender;
+        owner = msg.sender;
         setFixedPercent(_percent);
         addMember(msg.sender); // owner at 1st place in the member array
     }
 
     function deposit() public payable {
-        require(!reEntrancyMutex);
-        reEntrancyMutex= true; 
-        totalAmount= totalAmount.add(msg.value);
+        require (!reEntrancyMutex);
+        reEntrancyMutex = true; 
+        totalAmount = totalAmount.add(msg.value);
         emit OwnerDepositSuccess(msg.value);
-        reEntrancyMutex= false; 
+        reEntrancyMutex = false; 
     }
     
-    function setFixedPercent(uint256 _percent) onlyOwner public {
+    function setFixedPercent(uint256 _percent) public onlyOwner {
         require((0 <= _percent) && (_percent <= 100));
-        FIXED_PERCENT= _percent;
+        FIXED_PERCENT = _percent;
         emit ChangePercentSuccess(_percent);
     }
     
-    function ownerWithdraw(uint256 _amount) onlyOwner payable public {
+    function ownerWithdraw(uint256 _amount) public onlyOwner payable {
         require(!reEntrancyMutex);
         require(_amount <= totalAmount);
-        reEntrancyMutex= true;
-        totalAmount= totalAmount.sub(_amount);
+        reEntrancyMutex = true;
+        totalAmount = totalAmount.sub(_amount);
         owner.transfer(_amount);
         emit OwnerWithdrawSuccess(_amount);
-        reEntrancyMutex= false;
+        reEntrancyMutex = false;
     }
     
     function addMember(address _address) private {
         require(!whiteList[_address]);
         member.push(_address);
-        whiteList[_address]= true;
+        whiteList[_address] = true;
         emit AddMemberSuccess(_address, member.length-1);
     }
     
-    function createFixedAmount(address _address, uint256 _amount) private {
-        uint256 lockDuration= LOCK_DURATION; //in second
+    function createFixedAmount(address _address, uint256 _fixedAmount) private {
         
-        LockAtribute memory newAmount = LockAtribute({
-            time: now,
-            endTime: now + lockDuration,
-            value: _amount,
+        LockAtribute memory _fixed = LockAtribute({
+            startTime: now,
+            endTime: now + LOCK_DURATION,
+            value: _fixedAmount,
             lockStatus: StatusType.Locked
         });
         
-        fixedAmount[_address].push(newAmount);
+        fixedAmount[_address].push(_fixed);
     }
     
-    function createBonusAmount(address _address, uint256 _amount) private {
-        uint256 lockDuration= LOCK_DURATION; //in second
+    function createBonusAmount(address _address, uint256 _bonusAmount) private {
         
-        LockAtribute memory newAmount = LockAtribute({
-            time: now,
-            endTime: now + lockDuration,
-            value: _amount,
+        LockAtribute memory _bonus = LockAtribute({
+            startTime: now,
+            endTime: now + LOCK_DURATION,
+            value: _bonusAmount,
             lockStatus: StatusType.Locked
         });
         
-        bonusAmount[_address].push(newAmount);
+        bonusAmount[_address].push(_bonus);
     }
     
-    function createLockedAmount(address _address, uint256 _amount, uint256 _fixedPercent) onlyOwner public {
-        if (_fixedPercent != FIXED_PERCENT) setFixedPercent(_fixedPercent);
-        require(_amount <= totalAmount);
+    function createLockedAmount(address _address, uint256 _lockedAmount) public onlyOwner {
+        require(_lockedAmount <= totalAmount);
 
         if (!whiteList[_address]) addMember(_address);
 
-        uint256 newFixedAmount= _amount.mul(FIXED_PERCENT).div(100);
-        uint256 newBonusAmount= _amount.sub(newFixedAmount);
+        uint256 _fixedAmount = _lockedAmount.mul(FIXED_PERCENT).div(100);
+        uint256 _bonusAmount = _lockedAmount.sub(_fixedAmount);
         
-        createFixedAmount(_address, newFixedAmount);
-        createBonusAmount(_address, newBonusAmount);
+        createFixedAmount(_address, _fixedAmount);
+        createBonusAmount(_address, _bonusAmount);
         
-        totalAmount= totalAmount.sub(_amount);
-        lockedAmount[_address]= lockedAmount[_address].add(_amount);
-        emit CreatedSuccess(_address, _amount);
+        totalAmount = totalAmount.sub(_lockedAmount);
+        lockedAmount[_address] = lockedAmount[_address].add(_lockedAmount);
+        emit CreatedSuccess(_address, _lockedAmount);
     }
     
     function updateStatus(address _address) public {
-        for (uint256 i= 0; i< bonusAmount[_address].length; i++) {
+        for (uint256 i = 0; i < bonusAmount[_address].length; i++) {
             if ((bonusAmount[_address][i].lockStatus == StatusType.Locked) && 
-            (bonusAmount[_address][i].endTime < now)){
-                bonusAmount[_address][i].lockStatus= StatusType.Unlocked;
-                lockedAmount[_address]= lockedAmount[_address].sub(bonusAmount[_address][i].value);
-                unlockedAmount[_address]+= bonusAmount[_address][i].value;
+                (bonusAmount[_address][i].endTime < now)) {
+
+                bonusAmount[_address][i].lockStatus = StatusType.Unlocked;
+                lockedAmount[_address] = lockedAmount[_address].sub(bonusAmount[_address][i].value);
+                unlockedAmount[_address] += bonusAmount[_address][i].value;
             }
             
             if ((fixedAmount[_address][i].lockStatus == StatusType.Locked) && 
-            (fixedAmount[_address][i].endTime < now)){
-                fixedAmount[_address][i].lockStatus= StatusType.Unlocked;
-                lockedAmount[_address]=lockedAmount[_address].sub(fixedAmount[_address][i].value);
-                unlockedAmount[_address]+= fixedAmount[_address][i].value;
+                (fixedAmount[_address][i].endTime < now)) {
+
+                fixedAmount[_address][i].lockStatus = StatusType.Unlocked;
+                lockedAmount[_address] = lockedAmount[_address].sub(fixedAmount[_address][i].value);
+                unlockedAmount[_address] += fixedAmount[_address][i].value;
             }
         }
     }
     
-    function removeSpecificBonusAmount(address _address, uint256 _amountId) private returns(uint256) {
+    function removeSpecificBonusAmount(address _address, uint256 _id) private returns(uint256) {
         //if still removeable, remove and add amount into totalAmount    
-        if ((bonusAmount[_address][_amountId].lockStatus == StatusType.Locked) && (bonusAmount[_address][_amountId].time + BONUS_REMOVEALBE_DURATION > now)) {
-            uint256 value= bonusAmount[_address][_amountId].value;
-            bonusAmount[_address][_amountId].lockStatus= StatusType.Removed;
-            lockedAmount[_address]= lockedAmount[_address].sub(value);
+        if ((bonusAmount[_address][_id].lockStatus == StatusType.Locked) && 
+            (bonusAmount[_address][_id].startTime + BONUS_REMOVEALBE_DURATION > now)) {
+
+            uint256 value = bonusAmount[_address][_id].value;
+            bonusAmount[_address][_id].lockStatus = StatusType.Removed;
+            lockedAmount[_address] = lockedAmount[_address].sub(value);
             return value;
         }
         return 0;
     }
     
-    function removeBonusAmount(address _address, bool _isSpecific, uint256 _amountId) onlyOwner public {
+    function removeBonusAmount(address _address, bool _isSpecific, uint256 _id) public onlyOwner {
         require(whiteList[_address]);
-        uint256 removedAmount= 0;
+        uint256 removedAmount = 0;
         updateStatus(_address);
         
         if (_isSpecific) {
-            require((0 <= _amountId) && (_amountId <  bonusAmount[_address].length));
-            removedAmount= removeSpecificBonusAmount(_address, _amountId);
+            require((0 <= _id) && (_id <  bonusAmount[_address].length));
+            removedAmount = removeSpecificBonusAmount(_address, _id);
         } else {
             // remove all bonusAmount => search all bonusAmount sent to this _address
-            for (uint256 i= 0; i< bonusAmount[_address].length; i++) {
-                removedAmount= removedAmount.add(removeSpecificBonusAmount(_address, i));
+            for (uint256 i = 0; i< bonusAmount[_address].length; i++) {
+                removedAmount = removedAmount.add(removeSpecificBonusAmount(_address, i));
             }
         }
 
-        totalAmount= totalAmount.add(removedAmount);
+        totalAmount = totalAmount.add(removedAmount);
         emit RemovedSuccess(removedAmount);
     }
     
     //Members Functions
     
     function setUnlockedToWithdrawn() private {
-        address _address= msg.sender;
-        for (uint256 i= 0; i< bonusAmount[_address].length; i++) {
+        address _address = msg.sender;
+        for (uint256 i = 0; i < bonusAmount[_address].length; i++) {
             //if Unlocked
             if (bonusAmount[_address][i].lockStatus == StatusType.Unlocked) {
-                bonusAmount[_address][i].lockStatus= StatusType.Withdrawn;
+                bonusAmount[_address][i].lockStatus = StatusType.Withdrawn;
             }
             
             if (fixedAmount[_address][i].lockStatus == StatusType.Unlocked) {
-                fixedAmount[_address][i].lockStatus= StatusType.Withdrawn;
+                fixedAmount[_address][i].lockStatus = StatusType.Withdrawn;
             }
         }
     }
@@ -215,30 +214,27 @@ contract NextyBonus {
     function memberWithdraw() onlyMember public {
         require(!reEntrancyMutex);
 
-        address _address= msg.sender;
-        uint256 amount= unlockedAmount[_address]; // Withdraw amount
+        uint256 amount = unlockedAmount[msg.sender]; // Withdraw amount
         require(amount > 0);  
 
-        reEntrancyMutex= true;
+        reEntrancyMutex = true;
         
-        withdrawnAmount[_address]= withdrawnAmount[_address].add(amount);
-        unlockedAmount[_address]= 0;
+        withdrawnAmount[msg.sender] = withdrawnAmount[msg.sender].add(amount);
+        unlockedAmount[msg.sender] = 0;
         setUnlockedToWithdrawn();
-        _address.transfer(amount);
+        msg.sender.transfer(amount);
 
-        reEntrancyMutex= false; 
-        emit MemberWithdrawSuccess(_address, amount);
+        reEntrancyMutex = false; 
+        emit MemberWithdrawSuccess(msg.sender, amount);
     }
     
     //Public Functions
 
     function getLockedAmount(address _address) public view returns(uint256) {
-        //updateStatus(_address);
         return lockedAmount[_address];
     }
     
     function getUnlockedAmount(address _address) public view returns(uint256) {
-        //updateStatus(_address);
         return unlockedAmount[_address];
     }
     
@@ -246,39 +242,39 @@ contract NextyBonus {
         return withdrawnAmount[_address];
     }
     
-    function getFixedHistory(address _address, uint256 i) public view returns(uint256, uint256, uint256, uint256, bool){
-        require(i < fixedAmount[_address].length);
+    function getFixedHistory(address _address, uint256 _id) public view returns(uint256, uint256, uint256, uint256, bool){
+        require(_id < fixedAmount[_address].length);
         
-        uint256  time;
+        uint256  startTime;
         uint256 endTime;
         uint256  value;
         StatusType  lockStatus;
         bool removeable;
         
-        time= fixedAmount[_address][i].time;
-        endTime= fixedAmount[_address][i].endTime;
-        value= fixedAmount[_address][i].value;
-        lockStatus= fixedAmount[_address][i].lockStatus;
-        removeable= false;
+        startTime = fixedAmount[_address][_id].startTime;
+        endTime = fixedAmount[_address][_id].endTime;
+        value = fixedAmount[_address][_id].value;
+        lockStatus = fixedAmount[_address][_id].lockStatus;
+        removeable = false;
         
-        return (time, endTime, value, uint256(lockStatus), removeable);
+        return (startTime, endTime, value, uint256(lockStatus), removeable);
     }
     
-    function getBonusHistory(address _address, uint256 i) public view returns(uint256, uint256, uint256, uint256, bool){
-        require(i < bonusAmount[_address].length);
-        uint256  time;
+    function getBonusHistory(address _address, uint256 _id) public view returns(uint256, uint256, uint256, uint256, bool){
+        require(_id < bonusAmount[_address].length);
+        uint256  startTime;
         uint256 endTime;
         uint256  value;
         StatusType  lockStatus;
         bool removeable;
         
-        time= bonusAmount[_address][i].time;
-        endTime= bonusAmount[_address][i].endTime;
-        value= bonusAmount[_address][i].value;
-        lockStatus= bonusAmount[_address][i].lockStatus;
-        removeable= ((lockStatus == StatusType.Locked) && (time + BONUS_REMOVEALBE_DURATION > now));
+        startTime = bonusAmount[_address][_id].startTime;
+        endTime = bonusAmount[_address][_id].endTime;
+        value = bonusAmount[_address][_id].value;
+        lockStatus = bonusAmount[_address][_id].lockStatus;
+        removeable = ((lockStatus == StatusType.Locked) && (startTime + BONUS_REMOVEALBE_DURATION > now));
         
-        return (time, endTime, value, uint256(lockStatus), removeable);
+        return (startTime, endTime, value, uint256(lockStatus), removeable);
     }
 
     function getHistoryLength(address _address) public view returns(uint256){
