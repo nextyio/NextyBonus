62f7510dedf46b868c1832c6a31a3deb416ae5e8
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-08-20 09:02:13 +0700

    change text on sidebar

--- src/I18N/en.js
+++ src/I18N/en.js
@@ -10,7 +10,7 @@ export default {
     '0008': 'About',
     '0009': 'FAQ',
     '0010': 'Interest Management',
-    '0011': 'Set Percent',
+    '0011': 'Set Percentage',
     '0012': 'Setting Packages',
     '0013': 'Deposit',
 
